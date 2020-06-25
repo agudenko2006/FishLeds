bd8dc4087c1c5d11c2ed670c96aa768dfb1c71c7
agudenko2006 <agudenko2006@gmail.com>
2020-06-25 17:17:01 +0300

    old

--- readme.md
+++ readme.md
@@ -1,10 +1,16 @@
-# script options
-Use python3 main.py --help
-
-# install packages
-* pyserial
-* yeelight
-* colorama
+# Install
+1. Install arduino IDE
+2. Clone this project
+3. Open sketch.ino
+4. Upload sketch
+5. Open Serial monitor
+6. Send a255
+7. Close it
+8. Open "edit applications" and add utility.sh
+9. Check "Run in terminal"
+10. Save configuration
+11. Add some shortcuts to KDE
+shortcut command is script.sh width one of options
 
 # OPTIONS:
 * WHITE --- a255 (255 is brightness)
---
+++ utility.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+port="/dev/ttyUSB0"
+stty -F $port 9600
+
+clear
+echo "FizhLeds"
+# sleep 1s
+clear
+# echo "Hit CTRL+C to exit"
+# sleep 1s
+while true
+do
+	clear
+	echo "r - red"
+	echo "g - green"
+	echo "b - blue"
+	echo "c - hue wheel"
+	echo "a - all"
+	echo "1 - dark mode"
+	echo "2 - light mode"
+	echo "0 - exit"
+	read i
+	if [ $i -eq 0 ]
+	then
+		exit 0
+	elif [ $i -eq 1 ]
+	then
+		echo -ne r0g33b85 > $port 
+		lookandfeeltool -a org.kde.breezedark.desktop
+	elif [ $i -eq 2 ]
+	then
+		echo -ne r0g255b100 > $port 
+		lookandfeeltool -a org.kde.breeze.desktop
+	else
+		echo -ne $i > $port 
+		clear
+		echo "Enter color brightness"
+		read i
+		echo -ne $i > $port 
+		clear
+	fi
+done

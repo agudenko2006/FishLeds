4a6e93905f961dc4b0dbd1fd8c43cbad100863d0
agudenko2006 <agudenko2006@gmail.com>
2020-07-04 19:20:26 +0300

    Now you can disable console arguments

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+ledPort='/dev/ttyUSB0'
+lampIP='192.168.0.102'
+useConsole=False
+
+#------------------------------------------#
+
 import serial
 import psutil
 from random import random
 import sys
 from yeelight import Bulb
-lamp = Bulb("192.168.0.102")
+lamp = Bulb(lampIP)
 from time import sleep
 from colorama import Fore, Back
 
 # time = 0
 time = 0.02
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+ser = serial.Serial(ledPort, 9600)
 sleep(0.3)
 
 def huewheel():
@@ -96,31 +102,59 @@ def cpuLoad():
 		ser.write(b"g")
 		ser.write(str(201-load).encode('ascii'))
 
-if sys.argv[1] == 'on':
-	clear()
-elif sys.argv[1] == 'off':
-	ser.write(b'a0')
-elif sys.argv[1] == 'rgb':
-	ser.write(b"r")
-	ser.write(str(sys.argv[2]).encode('ascii'))
-	ser.write(b"g")
-	ser.write(str(sys.argv[3]).encode('ascii'))
-	ser.write(b"b")
-	ser.write(str(sys.argv[4]).encode('ascii'))
-elif sys.argv[1] == 'yeelight':
-	if sys.argv[2] == 'toggle':
-		lamp.toggle()
-	else:
-		lamp.set_color_temp(int(sys.argv[2]))
-elif sys.argv[1] == '--help':
-	print(Fore.WHITE, 'on - leds on\noff - leds off\nled_menu - menu')
-	print(Fore.WHITE, 'rgb (0 - 255) (0 - 255) (0 - 255) - set leds color to...\nyeelight toggle - toggle main lamp\nyeelight (value) - change color temperature')
-elif sys.argv[1] == 'led_menu':
+if useConsole == True:
+	if sys.argv[1] == 'on':
+		clear()
+	elif sys.argv[1] == 'off':
+		ser.write(b'a0')
+	elif sys.argv[1] == 'rgb':
+		ser.write(b"r")
+		ser.write(str(sys.argv[2]).encode('ascii'))
+		ser.write(b"g")
+		ser.write(str(sys.argv[3]).encode('ascii'))
+		ser.write(b"b")
+		ser.write(str(sys.argv[4]).encode('ascii'))
+	elif sys.argv[1] == 'yeelight':
+		if sys.argv[2] == 'toggle':
+			lamp.toggle()
+		else:
+			lamp.set_color_temp(int(sys.argv[2]))
+	elif sys.argv[1] == '--help':
+		print(Fore.WHITE, 'on - leds on\noff - leds off\nled_menu - menu')
+		print(Fore.WHITE, 'rgb (0 - 255) (0 - 255) (0 - 255) - set leds color to...\nyeelight toggle - toggle main lamp\nyeelight (value) - change color temperature')
+	elif sys.argv[1] == 'led_menu':
+		print(Fore.GREEN, 'Modes:')
+		print(Fore.BLUE, ' huewheel')
+		print(Fore.CYAN, ' cpu (CPU load)')
+		print(Fore.YELLOW, ' fire')
+		print(Fore.YELLOW, ' linux (yellow)')
+		print(Fore.RED, ' new year')
+		print(Fore.WHITE, ' clear')
+		print(Fore.BLACK, Back.RED, 'off')
+		print(Fore.RESET, Back.RESET,'select one: ')
+		startFX=input()
+		if startFX == 'huewheel':
+			huewheel()
+		elif startFX == 'clear':
+			clear()
+		elif startFX == 'fire':
+			fire(2)
+		elif startFX == 'new year':
+			newYear()
+		elif startFX == 'linux':
+			linuxColor()
+		elif startFX == 'cpu':
+			cpuLoad()
+		elif startFX == 'off':
+			ser.write(b'a0')
+else:
 	print(Fore.GREEN, 'Modes:')
 	print(Fore.BLUE, ' huewheel')
 	print(Fore.CYAN, ' cpu (CPU load)')
 	print(Fore.YELLOW, ' fire')
 	print(Fore.YELLOW, ' linux (yellow)')
+	print(Fore.YELLOW, ' yeelight toggle')
+	print(Fore.YELLOW, ' yeelight color')
 	print(Fore.RED, ' new year')
 	print(Fore.WHITE, ' clear')
 	print(Fore.BLACK, Back.RED, 'off')
@@ -136,6 +170,10 @@ elif sys.argv[1] == 'led_menu':
 		newYear()
 	elif startFX == 'linux':
 		linuxColor()
+	elif startFX == 'yeelight toggle':
+		lamp.toggle()
+	elif startFX == 'yeelight color':
+		lamp.set_color_temp(int(input()))
 	elif startFX == 'cpu':
 		cpuLoad()
 	elif startFX == 'off':
